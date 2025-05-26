14d679bc9ff89e06d400e47f94be288efced0b14
mmadc0der <tim110702@mail.ru>
2025-05-26 22:54:43 +0300

    feat: logger

---
+++ logger.py
@@ -0,0 +1,56 @@
+import logging
+from logging import StreamHandler, Formatter
+from pathlib import Path
+from datetime import datetime
+import os
+from logging.handlers import RotatingFileHandler
+
+class CustomRotatingFileHandler(RotatingFileHandler):
+    def doRollover(self):
+        if self.stream:
+            self.stream.close()
+            self.stream = None
+        # Shift old logs: .04.log <- .03.log <- ... <- .01.log
+        for i in range(self.backupCount - 1, 0, -1):
+            sfn = f"{self.baseFilename[:self.baseFilename.rfind('.log')]}_" + f"{i:#04x}"[2:] + ".log"
+            dfn = f"{self.baseFilename[:self.baseFilename.rfind('.log')]}_" + f"{i+1:#04x}"[2:] + ".log"
+            if os.path.exists(sfn):
+                if os.path.exists(dfn):
+                    os.remove(dfn)
+                os.rename(sfn, dfn)
+        # Rename current log to .1.log
+        dfn = f"{self.baseFilename[:self.baseFilename.rfind('.log')]}_01.log"
+        if os.path.exists(dfn):
+            os.remove(dfn)
+        if os.path.exists(self.baseFilename):
+            os.rename(self.baseFilename, dfn)
+        self.stream = self._open()
+
+def init_logging(file_level=logging.INFO, console_level=logging.WARNING, log_dir="log", log_file_name="experiment-orchestrator", log_format="%(asctime)s %(levelname)s [%(process)d:%(threadName)s] %(funcName)s: %(message)s"):
+    """
+    Initializes logging: root logger, console and file handler with rotation.
+    Logs are written to log/experiment-orchestrator_YYYYMMDD.log and archives experiment-orchestrator_YYYYMMDD.NN.log
+    """
+    log_formatter = Formatter(log_format)
+    logger = logging.getLogger()
+    logger.setLevel(file_level)
+
+    # Console Handler (WARNING)
+    console_handler = StreamHandler()
+    console_handler.setFormatter(log_formatter)
+    console_handler.setLevel(console_level)
+    logger.addHandler(console_handler)
+
+    # File Handler (INFO+), rotation by size, up to 5 archives
+    log_dir = Path(__file__).parent / log_dir
+    log_dir.mkdir(exist_ok=True)
+    log_file_base = log_dir / f"{log_file_name}_{datetime.now().strftime('%Y-%m-%d')}.log"
+    file_handler = CustomRotatingFileHandler(
+        str(log_file_base),
+        maxBytes=10*1024*1024,
+        backupCount=5
+    )
+    file_handler.setFormatter(log_formatter)
+    file_handler.setLevel(file_level)
+    logger.addHandler(file_handler)
+    logging.info("Logging configured: Console=%s, File=%s (%s)", console_level, file_level, str(log_file_base))
